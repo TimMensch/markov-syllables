dcbd889d908d3793feaac819bd4d2407d96ebf3f
Tim Mensch <tim-rz@bitgems.com>
2022-05-10 16:51:41 -0600

    Rewrite in TypeScript and fix bugs and tests

--- markov.ts
+++ markov.ts
@@ -1,18 +1,23 @@
-
 // randomly choose a letter based on the frequency statistics passed in.
 // letterStats contains a property for the total letter occurrences counted,
+
+import { syllable } from "syllable";
+
+import { LetterCount } from "./LetterCount";
+import { Stats } from "./Stats";
+
 // as well as properties for each individual letter count
-function chooseLetter(letterStats) {
+function chooseLetter(letterStats: LetterCount) {
     if (!letterStats) {
         throw "NoStats";
     }
     var draw = Math.floor(Math.random() * letterStats.total);
     var count = 0;
-    for(var letter in letterStats) {
-        if (letter === 'total') {
+    for (var letter in letterStats) {
+        if (letter === "total") {
             continue;
         }
-        count += letterStats[letter];
+        count += letterStats[letter] ?? 0;
         if (count > draw) {
             return letter;
         }
@@ -20,55 +25,43 @@ function chooseLetter(letterStats) {
     throw "NoStats"; // shouldn't get here with proper stats
 }
 
- syllable = require('syllable');
-
-
 // create a Markov word of length syllables based on the stats passed in.
 // the stats object contains counts of first letter occurrences,
 // second letter occurrences relative to the first letter,
 // letter occurrences relative to the preceding two letters,
 // second to last letter occurrences relative to the preceding two letters,
 // and last letter occurrences relative to the preceding two letters.
-function createWord(stats, syllables) {
-
+function createWord(stats: Stats, options: { syllables?: number; letters?: number }) {
     // start by choosing the first two letters (syllables is at least 3)
     var l1 = chooseLetter(stats.firstLetters),
-        l2 = chooseLetter(stats.secondLetters[l1]),
+        l2 = chooseLetter(stats.secondLetters[l1] as LetterCount),
         currentWord = l1 + l2,
         nextLetter,
-        syllable_count=0;
+        syllable_count = 0;
 
-   
-    while(syllable_count < syllables) {
-
-        nextLetter = chooseLetter(stats.letters[l1 + l2]);
+    while (
+        (options.syllables && syllable_count < options.syllables) ||
+        (options.letters && currentWord.length < options.letters)
+    ) {
+        nextLetter = chooseLetter(stats.letters[l1 + l2] as LetterCount);
         currentWord += nextLetter;
         l1 = l2;
         l2 = nextLetter;
 
-        syllable_count=syllable(currentWord);
+        syllable_count = syllable(currentWord);
         // console.log(syllable_count,currentWord)
     }
 
     return currentWord;
 }
 
-
 // entry point to generate the Markov word(s). syllables must be at least 3,
 // numberOfWords is optional and is 1 by default.
-function generate(stats, syllables, numberOfWords) {
-    if (arguments.length < 2 || arguments.length > 3) {
-        throw new Error("Incorrect number of arguments");
-    }
-    if (!stats.firstLetters || stats.firstLetters.total < 1) {
-        throw new Error("Invalid letter stats");
-    }
-    if (syllables < 2) {
-        throw new Error("Syllables must be at least 3 letters");
-    }
-    numberOfWords = numberOfWords || 1;
-    if (numberOfWords < 1) {
-        throw new Error("Invalid number of words");
+export function generate(stats: Stats, options: { syllables?: number; letters?: number; numberOfWords?: number }) {
+    const numberOfWords = options.numberOfWords ?? 1;
+
+    if (options.syllables == null && options.letters == null) {
+        throw new Error("Bad arguments");
     }
 
     // create the specified number of words. the letters
@@ -80,26 +73,23 @@ function generate(stats, syllables, numberOfWords) {
     // relatively rare.
     var result = [],
         retries = 0;
-    for(var i = 0; i < numberOfWords; i++) {
+    for (var i = 0; i < numberOfWords; i++) {
         try {
-            result.push(createWord(stats, syllables));
+            result.push(createWord(stats, options));
             retries = 0;
-        } catch(ex) {
+        } catch (ex) {
             if (ex === "NoStats") {
                 // occasionally word endings won't work out - retry
                 if (retries > 5) {
-                    throw new Error(
-                        "Error generating words - insufficient stats");
+                    throw new Error("Error generating words - insufficient stats");
                 }
                 retries++;
                 i--;
-            } else { // something else went wrong
+            } else {
+                // something else went wrong
                 throw ex;
             }
         }
     }
     return result;
 }
- 
-module.exports = {generate: generate};
-
